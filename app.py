e903b10a2d5aed94b3913dea255475e5cf179657
unknown <mohit.lda@gmail.com>
2022-11-26 12:36:13 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+# assignment *
+
+import streamlit as st
+
+
+st.title("Check number for Odd/Even")
+
+st.subheader("Write here the number")
+
+x = st.number_input("Enter Number")
+
+result = st.button("click here")
+if(result==True):
+    if(x%2==0):
+        st.write(f"""{x} *is Even Number*""")
+    else:
+        st.write(f"""{x} *is Odd Number*""")
+
+
